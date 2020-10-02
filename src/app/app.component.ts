a753557ec6d8ebb703d82fb8916c4a516fc83036
gary94746 <gary.109885@gmail.com>
2020-10-01 22:48:01 -0500

    style: setup background

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { PhotoService } from './photo.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,18 +7,7 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  title = 'gallery-fronted';
-  obs: Observable<any>;
-
   constructor(private readonly photoS: PhotoService) {}
 
-  ngOnInit(): void {
-    this.obs = this.photoS.getAll();
-  }
-
-  getAll() {}
-
-  getImageUrl(id: string, size: number) {
-    return `http://localhost:3000/photo/resize/?id=${id}&size=${size}`;
-  }
+  ngOnInit(): void {}
 }
