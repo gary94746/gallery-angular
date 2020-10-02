e9ce6f304bdc488c1dd4058b50e2e5da42b65bbe
gary94746 <gary.109885@gmail.com>
2020-10-01 23:29:47 -0500

    feat: navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PhotoRegisterComponent } from './photo-register/photo-register.component';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, PhotoRegisterComponent],
+  declarations: [AppComponent, PhotoRegisterComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
