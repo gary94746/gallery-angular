0926f8055d7c228eca08431ea779d514e9910531
gary94746 <gary.109885@gmail.com>
2020-10-03 00:48:21 -0500

    feat: get categories from api, add file field

--- src/app/photo-register/photo-register.component.ts
+++ src/app/photo-register/photo-register.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { PhotoService } from '../photo.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-photo-register',
   templateUrl: './photo-register.component.html',
-  styleUrls: ['./photo-register.component.scss']
+  styleUrls: ['./photo-register.component.scss'],
 })
 export class PhotoRegisterComponent implements OnInit {
+  $categories: Observable<any>;
+  label: string = '';
 
-  constructor() { }
+  constructor(private readonly photoService: PhotoService) {}
 
   ngOnInit() {
+    this.$categories = this.photoService.getCategories();
   }
 
+  handleFileInput(file: any) {
+    this.label = file[0].name;
+  }
 }
--- src/app/photo.service.ts
+++ src/app/photo.service.ts
@@ -5,15 +5,19 @@ import { HttpClient } from '@angular/common/http';
   providedIn: 'root',
 })
 export class PhotoService {
+  endPoint = 'http://localhost:3000/photo/';
+
   constructor(private readonly httpService: HttpClient) {}
 
   getAll(page: number, limit: number) {
-    return this.httpService.get(
-      `http://localhost:3000/photo/?page=${page}&limit=${limit}`
-    );
+    return this.httpService.get(`${this.endPoint}?page=${page}&limit=${limit}`);
   }
 
   imageUrl(id: string, size: string = '250') {
-    return `http://localhost:3000/photo/resize?id=${id}&size=${size}`;
+    return `${this.endPoint}resize?id=${id}&size=${size}`;
+  }
+
+  getCategories() {
+    return this.httpService.get(`http://localhost:3000/category`);
   }
 }
